77e407fca848f0765efe04e990228103d4adda72
Yunus Emre Acar <me@yunusacar.dev>
2024-01-14 17:12:57 +0300

    added health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import router from "./routes";
+import prisma from "./utils/prisma";
 
 config();
 
@@ -14,6 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api", router);
+app.get("/health", async (req, res) => {
+  try {
+    await prisma.$disconnect();
+    await prisma.$connect();
+    res.status(200).json({ message: "Server is running 🚀" });
+  } catch (err) {
+    res.status(500).json({ message: "Server is not running" });
+  }
+});
 
 // Server
 const port = process.env.PORT || 4000;
